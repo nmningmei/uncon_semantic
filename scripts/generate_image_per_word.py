dc50b0c5097719c7eb8337c1b53d800b18c927e2
Ning Mei <semsociaty@gmail.com>
2020-02-24 16:26:50 +0100

    Add files via upload

--- scripts/generate_image_per_word.py
+++ scripts/generate_image_per_word.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 18 16:03:27 2020
+Created on Tue Feb 18 16:11:23 2020
 
-@author: ning
+@author: nmei
 """
+
 import os
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 plt.style.use('dark_background')
 
@@ -14,24 +17,57 @@ figure_dir = '../stimuli_figure'
 if not os.path.exists(figure_dir):
     os.mkdir(figure_dir)
 
+dpi = 300
+image_size = 512
+fontsize = 20
+
 pathes = []
 for ii,row in df.iterrows():
-    row
-    fig,ax = plt.subplots(figsize = (4,4))
-    ax.text(0.5,0.5,row['Word'],
+    fig,ax = plt.subplots(figsize = (image_size/dpi,image_size/dpi))
+    ax.text(0.45,0.45,row['Word'].lower(),
             ha = 'center',
-            fontsize = 16,)
-    pathes.append(os.path.join(figure_dir,f'{row["English"]}.jpeg'))
-    fig.savefig(os.path.join(figure_dir,f'{row["English"]}.jpeg'),
-                dpi = 300,
-                bbox_inches = 'tight')
+            va = 'center',
+            fontsize = fontsize,
+            )
+    ax.axis('off')
+    plt.gca().set_aspect('equal', adjustable='box')
+    pathes.append(os.path.join(figure_dir,f'{row["English"].lower()}.jpeg'))
+    fig.savefig(os.path.join(figure_dir,f'{row["English"].lower()}.jpeg'),
+                dpi = dpi,
+#                bbox_inches = 'tight'
+                )
     plt.close('all')
 
-df['PATH'] = pathes
+df['PATH_spanish'] = pathes
+
+pathes = []
+for ii,row in df.iterrows():
+    fig,ax = plt.subplots(figsize = (image_size/dpi,image_size/dpi))
+    ax.text(0.45,0.45,row['English'].lower(),
+            ha = 'center',
+            va = 'center',
+            fontsize = fontsize,
+            )
+    ax.axis('off')
+    plt.gca().set_aspect('equal', adjustable='box')
+    pathes.append(os.path.join(figure_dir,f'{row["English"].upper()}.jpeg'))
+    fig.savefig(os.path.join(figure_dir,f'{row["English"].upper()}.jpeg'),
+                dpi = dpi,
+#                bbox_inches = 'tight'
+                )
+    plt.close('all')
+
+df['PATH_english'] = pathes
+
 columns = []
 for col in df.columns:
     if '\n' in col:
         col = col.replace('\n','_')
     columns.append(col)
 df.columns = columns
+
+# add varying blank period
+df['blank_dur'] = np.random.uniform(low = 0.3,high = 0.7,size = df.shape[0])
+df['category'] = df['Category'].map({'animal':'Living_Things',
+                                     'object':'Nonliving_Things'})
 df.to_csv('../data/sampled_words.csv',encoding = 'latin-1',index = False)
