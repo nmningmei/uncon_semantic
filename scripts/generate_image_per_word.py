14d6bf8b9bf5ed3a9ae90c1bf737e7e888734109
nmningmei <semsociaty@gmail.com>
2020-12-02 07:24:34 +0100

    update after a while

--- scripts/generate_image_per_word.py
+++ scripts/generate_image_per_word.py
@@ -14,20 +14,42 @@ figure_dir = '../stimuli_figure'
 if not os.path.exists(figure_dir):
     os.mkdir(figure_dir)
 
+fontsize = 16
+image_size_pixel = 512
+dpi = 300
+
 pathes = []
 for ii,row in df.iterrows():
     row
-    fig,ax = plt.subplots(figsize = (4,4))
-    ax.text(0.5,0.5,row['Word'],
+    fig,ax = plt.subplots(figsize = (image_size_pixel/dpi,image_size_pixel/dpi))
+    ax.text(0.45,0.45,row['Word'].lower(),
             ha = 'center',
-            fontsize = 16,)
-    pathes.append(os.path.join(figure_dir,f'{row["English"]}.jpeg'))
-    fig.savefig(os.path.join(figure_dir,f'{row["English"]}.jpeg'),
+            fontsize = fontsize,)
+    ax.axis('off')
+    pathes.append(os.path.join(figure_dir,f'{row["English"].lower()}.jpeg'))
+    fig.savefig(os.path.join(figure_dir,f'{row["English"].lower()}.jpeg'),
                 dpi = 300,
                 bbox_inches = 'tight')
     plt.close('all')
 
-df['PATH'] = pathes
+df['PATH_spanish'] = pathes
+
+#pathes = []
+#for ii,row in df.iterrows():
+#    row
+#    fig,ax = plt.subplots(figsize = (image_size_pixel/dpi,image_size_pixel/dpi))
+#    ax.text(0.45,0.45,row['English'].lower(),
+#            ha = 'center',
+#            fontsize = fontsize,)
+#    ax.axis('off')
+#    pathes.append(os.path.join(figure_dir,f'{row["English"].upper()}.jpeg'))
+#    fig.savefig(os.path.join(figure_dir,f'{row["English"].upper()}.jpeg'),
+#                dpi = 300,
+#                bbox_inches = 'tight')
+#    plt.close('all')
+#
+#df['PATH_english'] = pathes
+
 columns = []
 for col in df.columns:
     if '\n' in col:
@@ -35,3 +57,4 @@ for col in df.columns:
     columns.append(col)
 df.columns = columns
 df.to_csv('../data/sampled_words.csv',encoding = 'latin-1',index = False)
+
